23c1e6beab3025fb73704fe16cbd49e21ab46813
Wanijya <wanijyabisen@gmail.com>
2024-02-11 01:19:05 +0530

    CI

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,13 @@
+// Q. Accept the parameters and calculate the Compound Interest & print it on STDOUTQ.
+
 import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
-       int a = 5, b = 10;
-       int t = a;
-       a = b;
-       b = t;
-        System.out.println("a = "+ a);
-        System.out.println("b = "+ b);
+        Scanner sc = new Scanner(System.in);
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();
+        double n = sc.nextDouble();
+        double amount = p * Math.pow(((100+r)/100), n);
+        System.out.println(amount);
     }
 }
