b3785f016e603c7fdf20295e5b1032064fe9ae67
Wanijya <wanijyabisen@gmail.com>
2024-02-12 17:25:05 +0530

    If-else basic Que.

--- src/Demo.java
+++ src/Demo.java
@@ -1,13 +1,19 @@
-// Q. Accept the parameters and calculate the Compound Interest & print it on STDOUTQ.
+// Q.Accept two numbers and print the greatest between them
 
 import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double p = sc.nextDouble();
-        double r = sc.nextDouble();
-        double n = sc.nextDouble();
-        double amount = p * Math.pow(((100+r)/100), n);
-        System.out.println(amount);
+        System.out.println("Enter a 1st num: ");
+        int a = sc.nextInt();
+        System.out.println("Enter a 2nd num: ");
+        int b = sc.nextInt();
+        if (a > b){
+            System.out.println(a + " is greater.");
+        } else if (a < b) {
+            System.out.println(b + " is greater.");
+        } else {
+            System.out.println("Both are equal.");
+        }
     }
 }
