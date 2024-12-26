21ac4c93d58eaae0b529e39587f5b5870380af25
Wanijya <wanijyabisen@gmail.com>
2024-12-26 10:59:18 +0530

    Find the CI on any Principle.

--- src/Demo.java
+++ src/Demo.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the String you want to be reversed here..");
-        String name = sc.nextLine();
-        String rev = "";
-        int leng = name.length();
-        for (int i=leng-1; i>=0; i--){
-            rev = rev + name.charAt(i);
-        }
-        System.out.println("reverse is  " + rev);
+        System.out.println("Enter principle");
+        double principle = sc.nextDouble();
+        System.out.println("Enter rate");
+        double rate = sc.nextDouble();
+        System.out.println("Enter time in years");
+        double time = sc.nextDouble();
+        double interest = principle * Math.pow(((100+rate)/100),time);
+        System.out.println("Interest is " + interest);
     }
 }
